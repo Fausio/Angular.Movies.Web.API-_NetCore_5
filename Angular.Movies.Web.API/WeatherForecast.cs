6ff0ef15bf94cdf7233743751b7b17eb31d77816
fausio luis <fausioluis@live.com>
2021-05-21 14:05:26 +0200

    testing get request

--- Angular.Movies.Web.API/WeatherForecast.cs
+++ Angular.Movies.Web.API/WeatherForecast.cs
@@ -11,5 +11,12 @@ namespace Angular.Movies.Web.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+         public string name   { get; set; }
+
+    public WeatherForecast()
+        {
+            this.name = "Fausio code";
+        }
     }
 }
